933123aad1db913afa4f90f8d856e3a361179551
preet patel <preetpatelofficial004@gmail.com>
2025-05-02 15:15:48 -0400

    Fixed backend endpoints - Compatible with Qdrant & Langchain

--- api/main.py
+++ api/main.py
@@ -1,7 +1,9 @@
 import uvicorn
+from dotenv import load_dotenv
 
 def main():
     """Run the FastAPI application."""
+    load_dotenv()
     uvicorn.run("app:app", host="0.0.0.0", port=8080, reload=True)
 
 if __name__ == "__main__":
